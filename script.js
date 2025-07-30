a05d2dec7341171be4677d69caa09bea1abbe25a
arforU <arforua@gmail.com>
2025-07-30 10:09:39 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showPage(pageId) {
+  const pages = document.querySelectorAll('.page');
+  pages.forEach(page => page.style.display = 'none');
+
+  const selectedPage = document.getElementById(pageId);
+  if (selectedPage) {
+    selectedPage.style.display = 'block';
+  }
+}
